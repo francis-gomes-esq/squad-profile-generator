7e130202b1548cd5f1ccac31c33a305bfaaef2c1
francis-gomes-esq <figomesokoye@protonmail.com>
2024-02-25 20:01:49 +0000

    add missing params across classes and update package.json

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ const outputPath = path.join(OUTPUT_DIR, 'team.html');
 const render = require('./src/page-template.js');
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const team = [];
+
+const managerInfo = () => {
+	inquirer.prompt([
+		{
+			type: 'input',
+			name: 'name',
+			message: "Enter the Manager's name: ",
+		},
+		{
+			type: 'input',
+			name: 'id',
+			message: "Enter the Manager's ID:",
+		},
+		{
+			type: 'input',
+			name: 'email',
+			message: "Enter the Manager's email address:",
+		},
+		{
+			type: 'input',
+			name: 'officeNumber',
+			message: "Enter the Manager's office number:",
+		},
+	]);
+};
