e0ea5ff21471b3e65d2d73858966d2b54164d3f9
francis-gomes-esq <figomesokoye@protonmail.com>
2024-02-25 20:21:31 +0000

    add comments, manager information and code block for adding team members

--- index.js
+++ index.js
@@ -10,30 +10,43 @@ const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
 const render = require('./src/page-template.js');
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Array to store team members
 const team = [];
 
+// Function to gather information about the manager
 const managerInfo = () => {
-	inquirer.prompt([
-		{
-			type: 'input',
-			name: 'name',
-			message: "Enter the Manager's name: ",
-		},
-		{
-			type: 'input',
-			name: 'id',
-			message: "Enter the Manager's ID:",
-		},
-		{
-			type: 'input',
-			name: 'email',
-			message: "Enter the Manager's email address:",
-		},
-		{
-			type: 'input',
-			name: 'officeNumber',
-			message: "Enter the Manager's office number:",
-		},
-	]);
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'name',
+				message: "Enter the Manager's name: ",
+			},
+			{
+				type: 'input',
+				name: 'id',
+				message: "Enter the Manager's ID:",
+			},
+			{
+				type: 'input',
+				name: 'email',
+				message: "Enter the Manager's email address:",
+			},
+			{
+				type: 'input',
+				name: 'officeNumber',
+				message: "Enter the Manager's office number:",
+			},
+		])
+		// This block adds a manager to the team array and then adds more team members.
+		.then(answer => {
+			const manager = new manager(
+				answer.name,
+				answer.id,
+				answer.email,
+				answer.officeNumber
+			);
+			team.push(manager);
+			addTeamMemeber();
+		});
 };
