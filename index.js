9e482ea73e401f43dee764dfe9e65acffcf08db9
francis-gomes-esq <152029704+francis-gomes-esq@users.noreply.github.com>
2024-02-26 20:35:37 +0000

    Feature/squad profile generator (#3)
    * add link to mit licence to readme
    
    * add intern.js class
    
    * add module exports to intern.js class
    
    * add manager.js class
    
    * add missing params across classes and update package.json
    
    * add comments, manager information and code block for adding team members
    
    * update add team memeber
    
    * add function to get manager's information
    
    * add comment to get engineer's function
    
    * add function to get intern's information
    
    * add comment to get intern's function
    
    * gather manager information This commit checks if output directory exist and writed HTML content to file
    
    * add comment to function  This commit is about a funtion that creates HTML file Checks if output directory exists Creates it if it does not exist
    
    * fix typo
    
    * fix issues common to all classes  And revert license to ISC in package.JSON

--- index.js
+++ index.js
@@ -1,15 +1,188 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./src/page-template.js");
+const render = require('./src/page-template.js');
 
+// Array to store team members
+const team = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Function to gather information about the manager
+const managerData = () => {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'name',
+				message: "Enter the Manager's name: ",
+			},
+			{
+				type: 'input',
+				name: 'id',
+				message: "Enter the Manager's ID:",
+			},
+			{
+				type: 'input',
+				name: 'email',
+				message: "Enter the Manager's email address:",
+			},
+			{
+				type: 'input',
+				name: 'officeNumber',
+				message: "Enter the Manager's office number:",
+			},
 
+			// This block adds a manager to the team array and then adds more team members.
+		])
+		.then(answer => {
+			const manager = new Manager(
+				answer.name,
+				answer.id,
+				answer.email,
+				answer.officeNumber
+			);
+			team.push(manager);
+			addTeamMember();
+		});
+};
+
+const addTeamMember = () => {
+	inquirer.prompt(
+		[
+			{
+				type: 'confirm',
+				name: 'check',
+				message: 'Add a team member:',
+			},
+		].then(response => {
+			if (response.check) {
+				inquirer
+					.prompt([
+						{
+							type: 'List',
+							name: 'addMember',
+							choices: ['Engineer', 'Intern', 'Complete'],
+						},
+					])
+					.then(answer => {
+						if (answer.addMember === 'Engineer') {
+							engineerInfo();
+						} else if (answer.addMember === 'Intern') {
+							internInfo();
+						} else {
+							createHtml();
+						}
+					});
+			} else {
+				createHTML;
+			}
+		})
+	);
+};
+
+// Function to gather information about an engineer
+const engineerData = () => {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'name',
+				message: "Enter the Engineer's name:",
+			},
+
+			{
+				type: 'input',
+				name: id,
+				message: "Enter the Engineer's ID",
+			},
+
+			{
+				type: 'input',
+				name: 'email',
+				message: "Enter the Engineer's email",
+			},
+
+			{
+				type: 'input',
+				name: 'github',
+				message: "Enter the Engineer's github",
+			},
+		])
+		.then(answer => {
+			const engineer = new Engineer(
+				answer.name,
+				answer.id,
+				answer.email,
+				answer.github
+			);
+			team.push(engineer);
+			addTeamMember();
+		});
+};
+// Function to gather information about an intern
+const internData = () => {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'name',
+				message: "Enter the Intern's name:",
+			},
+
+			{
+				type: 'input',
+				name: 'id',
+				message: "Enter the Intern's ID:",
+			},
+
+			{
+				type: 'input',
+				name: 'email',
+				message: "Enter the Intern's email",
+			},
+
+			{
+				type: 'input',
+				name: 'github',
+				message: "Enter the Intern's github",
+			},
+		])
+		.then(answer => {
+			const intern = new Intern(
+				answer.name,
+				answer.id,
+				answer.email,
+				answer.github
+			);
+			team.push(intern);
+			addTeamMember();
+		});
+};
+
+managerData();
+
+// Function to create HTML file
+const createHTML = () => {
+	const htmlContent = render(team);
+
+	// Check if output directory exists, if not, create it
+	if (!fs.existsSync(OUTPUT_DIR)) {
+		fs.mkdirSync(OUTPUT_DIR);
+	}
+
+	// Write HTML content to file
+	fs.writeFile(outputPath, htmlContent, err => {
+		if (err) {
+			console.log(err);
+		}
+		return;
+	});
+
+	console.log('Your HTML is created');
+};
